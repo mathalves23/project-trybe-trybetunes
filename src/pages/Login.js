512fa35cd085e5b625853c926f8c1037daac5b28
Matheus Alves <matheus.aalves@hotmail.com>
2022-01-14 11:42:17 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom'; // FONTE: https://v5.reactrouter.com/web/api/Redirect
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nameLogin: '',
+      isLoading: false,
+      isButtonDisabled: true,
+      redirectEnable: false,
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    const MIN_CHAR = 3;
+    const activateButton = value.length >= MIN_CHAR;
+    this.setState({
+      nameLogin: value,
+      isButtonDisabled: !activateButton,
+    });
+  }
+
+  handleClick = () => {
+    const { nameLogin } = this.state;
+    this.setState({ isLoading: true });
+    createUser({ name: nameLogin }).then(() => {
+      this.setState({ isLoading: false, redirectEnable: true });
+    });
+  }
+
   render() {
+    const { nameLogin, isLoading, isButtonDisabled, redirectEnable } = this.state;
     return (
       <div data-testid="page-login">
-        <p>Login</p>
+        <form>
+          <label htmlFor="nameLogin">
+            <input
+              type="text"
+              name="nameLogin"
+              placeholder="Nome de Usuário"
+              data-testid="login-name-input"
+              id="nameLogin"
+              value={ nameLogin }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            onClick={ this.handleClick }
+            disabled={ isButtonDisabled }
+          >
+            Entrar
+          </button>
+          {/* Verifições com operador ternário */}
+          { isLoading ? <p>Carregando...</p> : null }
+          { redirectEnable ? <Redirect to="/search" /> : null}
+        </form>
       </div>
     );
   }

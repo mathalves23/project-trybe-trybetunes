db4d917395e6b1c62e3a8ac405293b17eeb572d8
Matheus Alves <matheus.aalves@hotmail.com>
2022-01-15 17:59:22 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -20,11 +21,36 @@ class Header extends React.Component {
   render() {
     const { nameLogin } = this.state;
     return (
-      <div data-testid="header-component">
-        {nameLogin ? <span data-testid="header-user-name">{ nameLogin }</span>
-          : (<p>Carregando...</p>)}
-        <p>Aqui está o Header</p>
-      </div>
+      <>
+        <div data-testid="header-component">
+          {nameLogin ? <span data-testid="header-user-name">{ nameLogin }</span>
+            : (<p>Carregando...</p>)}
+        </div>
+        <div>
+          <nav>
+            <Link
+              data-testid="link-to-search"
+              to="/search"
+            >
+              Search
+            </Link>
+            <Link
+              data-testid="link-to-favorites"
+              to="/favorites"
+            >
+              Favorites
+            </Link>
+            <Link
+              data-testid="link-to-profile"
+              to="/profile"
+            >
+              Profile
+
+            </Link>
+            {/* FONTE: https://v5.reactrouter.com/web/api/Link */}
+          </nav>
+        </div>
+      </>
     );
   }
 }
